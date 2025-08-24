93a4f77a534086c7600e4142c29acceff92740c1
Marian85Tudor <marian85tudor@gmail.com>
2025-08-24 15:18:13 +0200

    Runs 15 times now

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-      for (int i= 0;i<10;i++){
+      for (int i= 0;i<15;i++){
           System.out.println("Hello World ");
           Thread.sleep(2000);
       }
